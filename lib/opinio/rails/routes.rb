9437fbdcd89fcd9d005cb1c6a5fe756a847e63c0
Luiz Felipe <luiz.felipe.gp@gmail.com>
2011-05-10 15:59:07 -0300

    fixed routes for opinio

--- lib/opinio/rails/routes.rb
+++ lib/opinio/rails/routes.rb
@@ -1,11 +1,11 @@
 module ActionDispatch::Routing
   class Mapper
     def opinio(*args)
-      route_name    = args.first || Opinio.model_name.pluralize.downcase
       options       = args.extract_options!.symbolize_keys
+      route_name    = options[:path_name] || Opinio.model_name.pluralize.downcase
       options[:controller] ||= 'opinio/comments'
 
-      get "#{ route_name }(/:page)" => "#{options[:controller]}#index", :as => :comments 
+      get "#{ route_name }(/:page)" => "#{options[:controller].to_s}#index", :as => :comments 
     end
 
     def opinio_model(*args)
