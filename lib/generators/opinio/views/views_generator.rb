75692f6b207e5f035e5c9f6874598b41aba0b72f
eicca <wtltl2@gmail.com>
2012-04-18 14:47:13 +0400

    added haml views

--- lib/generators/opinio/views/views_generator.rb
+++ lib/generators/opinio/views/views_generator.rb
@@ -2,10 +2,20 @@ module Opinio
   module Generators
     class ViewsGenerator < Rails::Generators::Base
 
-      source_root File.expand_path('../../../../../app/views', __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
+
+      class_option :haml, :desc => 'Generate HAML views instead of ERB.', :type => :boolean
 
       def copy_views
-        directory "opinio", "app/views/opinio"
+        view_names = ["_comment", "_comments", "_new", "index"]
+        view_names.each do |view_name|
+          copy_file "#{view_language}/#{view_name}.html.#{view_language}", "app/views/opinio/comments/#{view_name}.html.#{view_language}"
+        end
+        directory "js", "app/views/opinio/comments"
+      end
+
+      def view_language
+        options.haml? ? 'haml' : 'erb'
       end
 
     end
