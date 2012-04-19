c2d42b11d269e3a172f494e132139d6219ac4468
eicca <wtltl2@gmail.com>
2012-04-19 15:45:08 +0400

    moved haml views to app/views/opinio/comments

--- lib/generators/opinio/views/views_generator.rb
+++ lib/generators/opinio/views/views_generator.rb
@@ -2,21 +2,25 @@ module Opinio
   module Generators
     class ViewsGenerator < Rails::Generators::Base
 
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../../../../app/views/opinio/comments', __FILE__)
 
       class_option :haml, :desc => 'Generate HAML views instead of ERB.', :type => :boolean
 
       def copy_views
-        view_names = ["_comment", "_comments", "_new", "index"]
-        view_names.each do |view_name|
-          copy_file "#{view_language}/#{view_name}.html.#{view_language}", "app/views/opinio/comments/#{view_name}.html.#{view_language}"
+        html_names = ["_comment", "_comments", "_new", "index"]
+        js_names = ["create", "destroy", "reply"]
+        html_names.each do |name|
+          copy_file "#{name}.html.#{view_language}", "app/views/opinio/comments/#{name}.html.#{view_language}"
         end
-        directory "js", "app/views/opinio/comments"
+        js_names.each do |name|
+          copy_file "#{name}.js.erb", "app/views/opinio/comments/#{name}.js.erb"
+        end
       end
 
       def view_language
         options.haml? ? 'haml' : 'erb'
       end
+
 
     end
   end
